b31fb5c288443fbed15dd4f019e2d094a313609d
xcatliu <xcatliu@gmail.com>
2015-05-25 21:58:38 +0800

    Update 005

--- problems/005-longest-palindromic-substring/index.js
+++ problems/005-longest-palindromic-substring/index.js
@@ -1,5 +1,16 @@
 // https://leetcode.com/problems/longest-palindromic-substring/
 
 var longestPalindromicSubstring = module.exports = function(s) {
-  return s;
+  var i;
+  for (var i = 0; i < n - 1; i++) {
+    var p1 = expandAroundCenter(s, i, i);
+    if (p1.length > longest.length) {
+      longest = p1;
+    }
+    var p2 = expandAroundCenter(s, i, i + 1);
+    if (p2.length > longest.length) {
+      longest = p2;
+    }
+  }
+  return longest;
 };
