0a2cca9368612f85d93d41ce06174b455d4db03a
xcatliu <xcatliu@gmail.com>
2015-07-15 22:06:54 +0800

    modify 005

--- problems/005-longest-palindromic-substring/index.js
+++ problems/005-longest-palindromic-substring/index.js
@@ -1,20 +1,42 @@
-// https://leetcode.com/problems/longest-palindromic-substring/
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ *
+ * Given a string S, find the longest palindromic substring in S.
+ * You may assume that the maximum length of S is 1000,
+ * and there exists one unique longest palindromic substring.
+ */
+
+/**
+ * 相当于将 s 字符之间插入 #
+ * banana => #b#a#n#a#n#a#
+ * abba => #a#b#b#a#
+ * abcd => #a#b#c#d#
+ * 这样处理的好处是 aba 和 abba 的两种情况可以一起处理
+ */
+function get(s, i) {
+  if (i % 2 === 0) return '#';
+  return s[(i - 1) / 2];
+}
 
 var longestPalindromicSubstring = module.exports = function(s) {
-  var n = s.length;
-  if (n == 0) {
-    return '';
-  }
-  var i;
-  for (var i = 0; i < n - 1; i++) {
-    var p1 = expandAroundCenter(s, i, i);
-    if (p1.length > longest.length) {
-      longest = p1;
+  var length = s.length;
+  if (length == 0) return '';
+  var result;
+  var longest = 0;
+  // 2 * length + 1 是 #b#a#n#a#n#a# 的长度
+  for (var i = 1; i < 2 * length; i++) {
+    // count 表示以当前字符为中心的回文字符的长度
+    var count = 0;
+    var j = 1;
+    while (i - j >= 0 && i + j <= 2 * length && get(s, i - j) === get(s, i + j)) {
+      count++;
+      j++;
     }
-    var p2 = expandAroundCenter(s, i, i + 1);
-    if (p2.length > longest.length) {
-      longest = p2;
+    console.log(i, count);
+    if (count > longest) {
+      result = s.slice((i - 1) / 2 - count, count);
+      longest = count;
     }
   }
-  return longest;
+  return result;
 };
