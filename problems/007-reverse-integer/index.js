95f0ab14ed2d2753216d439f76fbb9a41eeeddbb
xcatliu <xcatliu@gmail.com>
2015-07-28 16:53:19 +0800

    Complete 007

--- problems/007-reverse-integer/index.js
+++ problems/007-reverse-integer/index.js
@@ -22,10 +22,19 @@
  * reversed integer overflows.
  */
 
-/**
- * @param {number} x
- * @return {number}
- */
-var reverse = function(x) {
-
+ /**
+  * @param {number} x
+  * @return {number}
+  */
+var reverse = module.exports = function (x) {
+  var pn = x >= 0 ? '' : '-';
+  var num = Math.abs(x);
+  var result = Number(pn + num.toString().split('').reverse().join(''));
+  if (result > Math.pow(2, 31)) {
+    return 0;
+  }
+  if (result < 1 - Math.pow(2, 31)) {
+    return 0;
+  }
+  return result;
 };
