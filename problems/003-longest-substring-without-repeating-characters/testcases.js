4547e7a1bc122312ce092901b81a9de900af3c62
xcatliu <xcatliu@gmail.com>
2015-08-18 16:31:38 +0800

    Update 003 testcases

---
+++ problems/003-longest-substring-without-repeating-characters/testcases.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    input: ['abcabcbb'],
+    output: 3,
+  },
+  {
+    input: ['bbbbb'],
+    output: 1,
+  },
+  {
+    input: ['c'],
+    output: 1,
+  },
+  {
+    input: ['au'],
+    output: 2,
+  },
+];
