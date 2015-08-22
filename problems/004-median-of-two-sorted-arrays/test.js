ec8d9bd2d21b8685de6f78337206f0c4adbca53b
xcatliu <xcatliu@gmail.com>
2015-08-22 16:42:38 +0800

    Update 004 testcases

--- problems/004-median-of-two-sorted-arrays/test.js
+++
@@ -1,7 +0,0 @@
-require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-  var lineSplit = line.split(', ');
-  var nums1 = eval(lineSplit[0]);
-  var nums2 = eval(lineSplit[1]);
-  var result = require('./index')(nums1, nums2);
-  console.log(result);
-});
---
+++ problems/004-median-of-two-sorted-arrays/testcases.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    input: [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
+    output: 3,
+  },
+  {
+    input: [[], [1]],
+    output: 1,
+  },
+  {
+    input: [[], [2, 3]],
+    output: 2.5,
+  },
+  {
+    input: [[1, 2], [1, 2]],
+    output: 1.5,
+  },
+];
