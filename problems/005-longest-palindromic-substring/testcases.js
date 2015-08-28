b52d6c1febc48d4edc60c9769e78b9c337f2d823
xcatliu <xcatliu@gmail.com>
2015-08-28 16:52:24 +0800

    Update 005 testcases

---
+++ problems/005-longest-palindromic-substring/testcases.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    input: ['banana'],
+    output: 'anana',
+  },
+  {
+    input: ['a'],
+    output: 'a',
+  },
+  {
+    input: ['aaa'],
+    output: 'aaa',
+  },
+  {
+    input: ['ceabadabac'],
+    output: 'abadaba',
+  },
+];
