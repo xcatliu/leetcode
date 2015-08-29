35dbbbbd72920de84261486ba9df6850dc0504b4
xcatliu <xcatliu@gmail.com>
2015-08-29 16:54:28 +0800

    Update 006 testcases

--- problems/006-zigzag-conversion/test.js
+++
@@ -1,5 +0,0 @@
-require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-  var lineSplit = line.split(' ');
-  var result = require('./index')(lineSplit[0], lineSplit[1]);
-  console.log(result);
-});
---
+++ problems/006-zigzag-conversion/testcases.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    input: ['PAYPALISHIRING', 3],
+    output: 'PAHNAPLSIIGYIR',
+  },
+  {
+    input: ['AB', 1],
+    output: 'AB',
+  },
+];
