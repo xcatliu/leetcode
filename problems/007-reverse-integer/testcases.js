6ee93f2f7a1077155a605578aab1faec196a09ae
xcatliu <xcatliu@gmail.com>
2015-08-31 16:56:54 +0800

    Update 007 testcases

---
+++ problems/007-reverse-integer/testcases.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    input: [123],
+    output: 321,
+  },
+  {
+    input: [-123],
+    output: -321,
+  },
+  {
+    input: [100],
+    output: 1,
+  },
+  {
+    input: [1534236469],
+    output: 0,
+  },
+  {
+    input: [-2147483648],
+    output: 0,
+  },
+  {
+    input: [1463847412],
+    output: 2147483641,
+  },
+];
