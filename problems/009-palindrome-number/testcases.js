80d42791f077d2f6e9a25d1ca181f52f73a82a6e
xcatliu <xcatliu@gmail.com>
2015-09-02 17:01:24 +0800

    Update 008 testcases

---
+++ problems/009-palindrome-number/testcases.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    input: [1],
+    output: true,
+  },
+  {
+    input: [-1],
+    output: false,
+  },
+  {
+    input: [0],
+    output: true,
+  },
+  {
+    input: [121],
+    output: true,
+  },
+  {
+    input: [1001],
+    output: true,
+  },
+];
