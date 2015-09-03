2c15151cd46cfe75c7c6728b962462cb04cd9764
xcatliu <xcatliu@gmail.com>
2015-09-03 17:04:15 +0800

    Update 010 testcases

--- problems/010-regular-expression-matching/test.js
+++
@@ -1,6 +0,0 @@
-require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-  if (line.length === 0) return;
-  var lineSplit = line.split(' ');
-  var result = require('./index')(lineSplit[0], lineSplit[1]);
-  console.log(result);
-});
---
+++ problems/010-regular-expression-matching/testcases.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    input: ['aa a'],
+    output: false,
+  },
+  {
+    input: ['aa aa'],
+    output: true,
+  },
+  {
+    input: ['aaa aa'],
+    output: false,
+  },
+  {
+    input: ['aa a*'],
+    output: true,
+  },
+  {
+    input: ['aa .*'],
+    output: true,
+  },
+  {
+    input: ['ab .*'],
+    output: true,
+  },
+  {
+    input: ['aab c*a*b'],
+    output: true,
+  },
+  {
+    input: ['bbba .*a*a'],
+    output: true,
+  },
+];
