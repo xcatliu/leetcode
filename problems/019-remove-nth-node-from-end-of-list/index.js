a4c558993ff88221f1f3a68cb600ef329b37548a
xcatliu <xcatliu@gmail.com>
2015-10-09 15:15:10 +0800

    Add 019

---
+++ problems/019-remove-nth-node-from-end-of-list/index.js
@@ -0,0 +1,51 @@
+/**
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ *
+ * Given a linked list,
+ * remove the nth node from the end of list and return its head.
+ *
+ * For example,
+ *
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ *
+ * After removing the second node from the end, the linked list becomes
+ * 1->2->3->5.
+ *
+ * Note:
+ *
+ * Given n will always be valid.
+ * Try to do this in one pass.
+ */
+
+/**
+ * You should comment the following function to pass the leetcode tests.
+ * Definition for singly-linked list.
+ */
+var ListNode = require('../../structures/ListNode');
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = module.exports = function (head, n) {
+  if (n === 0) return head;
+  var stack = [];
+  var node = head;
+  while (node !== null) {
+    stack.push(node);
+    node = node.next;
+  }
+  if (stack.length - n - 1 === -1) {
+    return head.next;
+  }
+  stack[stack.length - n - 1].next = stack[stack.length - n + 1];
+  return head;
+};
---
+++ problems/019-remove-nth-node-from-end-of-list/testcases.js
@@ -0,0 +1,12 @@
+var ListNode = require('../../structures/ListNode');
+
+module.exports = [
+  {
+    input: [ListNode.generateList(54321), 2],
+    output: ListNode.generateList(5321),
+  },
+  {
+    input: [ListNode.generateList(1), 1],
+    output: null,
+  },
+];
