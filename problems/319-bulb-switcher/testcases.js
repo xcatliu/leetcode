befd8ed9f89814114a74d82e9010d9b7f449d4ab
xcatliu <xcatliu@gmail.com>
2015-11-14 17:52:14 +0800

    Update 319 testcases

--- problems/319-bulb-switcher/testcases.js
+++ problems/319-bulb-switcher/testcases.js
@@ -1,26 +1,2 @@
 module.exports = [
-  {
-    input: ['anagram', 'nagaram'],
-    output: true,
-  },
-  {
-    input: ['', 'a'],
-    output: false,
-  },
-  {
-    input: ['cat', 'let'],
-    output: false,
-  },
-  {
-    input: ['', ''],
-    output: true,
-  },
-  {
-    input: ['b', ''],
-    output: false,
-  },
-  {
-    input: ['a', 'a'],
-    output: true,
-  },
 ];
