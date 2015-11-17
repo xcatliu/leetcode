83d24cb4b99fad07b2d83e75d9d3fa8b6bac6a23
xcatliu <xcatliu@gmail.com>
2015-11-17 17:53:23 +0800

    Add 030

---
+++ problems/030-substring-with-concatenation-of-all-words/index.js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+ *
+ * You are given a string, s, and a list of words, words,
+ * that are all of the same length.
+ *
+ * Find all starting indices of substring(s) in s that is a concatenation of
+ * each word in words exactly once and without any intervening characters.
+ *
+ * For example, given:
+ *
+ * s: "barfoothefoobarman"
+ * words: ["foo", "bar"]
+ *
+ * You should return the indices: [0,9].
+ * (order does not matter).
+ */
+
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = module.exports = function (s, words) {
+};
